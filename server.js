c87c7552f4a67933db7be1c8c230903daa29ef30
Abdikalik Dini <adini18@gmail.com>
2023-08-29 11:15:31 -0400

    fixed issue where notes were not saving

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware for handling JSON data
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'Develop/public')));
 
-// API Endpoint to retrieve notes
+// Endpoint to retrieve notes
 app.get('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'Develop/db/notes.json'), 'utf8'));
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'Develop/db/db.json'), 'utf8'));
     res.json(notes);
 });
 
-// API Endpoint to save notes
+// Endpoint to save notes
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'Develop/db/notes.json'), 'utf8'));
-    newNote.id = notes.length > 0 ? notes[notes.length - 1].id + 1 : 1;  // Assigning an ID
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'Develop/db/db.json'), 'utf8'));
+    newNote.id = notes.length > 0 ? notes[notes.length - 1].id + 1 : 1;  
     notes.push(newNote);
-    fs.writeFileSync(path.join(__dirname, 'Develop/db/notes.json'), JSON.stringify(notes));
+    fs.writeFileSync(path.join(__dirname, 'Develop/db/db.json'), JSON.stringify(notes));
     res.json(newNote);
 });
 
@@ -30,11 +30,16 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
 });
 
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack); 
+    res.status(500).json({ error: 'Something went wrong!' }); 
 });
